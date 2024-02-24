d32b4997b3f9047b1fcc383a24548db0d6dd46ce
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-02-24 22:51:19 +0200

    header style update

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,6 +1,8 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
 (() => {
   const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-menu-open');
+  const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
@@ -11,11 +13,9 @@
 
     mobileMenu.classList.toggle('is-open');
 
-    // const scrollLockMethod = !isMenuOpen
-    //   ? 'disableBodyScroll'
-    //   : 'enableBodyScroll';
-
-    // bodyScrollLock[scrollLockMethod](document.body);
+    !isMenuOpen
+      ? disableBodyScroll(document.body)
+      : enableBodyScroll(document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -24,9 +24,8 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
-
     mobileMenu.classList.remove('is-open');
     openMenuBtn.setAttribute('aria-expanded', false);
-    // bodyScrollLock.enableBodyScroll(document.body);
+    enableBodyScroll(document.body);
   });
 })();
